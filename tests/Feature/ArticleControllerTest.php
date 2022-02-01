76e841a272401a16637fd8be74580294f0256ecd
yuta-yatsuka <yuta.yatsuka@ponion.jp>
2022-02-01 15:26:40 +0900

    masterにマージする前にCircleCIのテストを通過することがマストになっていることを検証するための変更

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -15,7 +15,7 @@ class ArticleControllerTest extends TestCase
     public function testIndex(){
         $response = $this->get(route('articles.index'));
 
-        $response->assertStatus(200)
+        $response->assertStatus(400)
             ->assertViewIs('articles.index');
     }
 
